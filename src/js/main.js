1d555c8f77048ff1fc0715381f49cb89949ba5ab
Klim Klimentov <klimentov@i.ua>
2021-10-14 01:38:27 +0300

    ## Task # 0 Done created a function that creates the necessary objects. The function returns data with the required typing. Created two objects player1 and player2

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,12 @@
 "use strict";
-console.log('Hello World!');
+function createPlayer  (name,hp,img,weapon=[] ){
+
+    return{name:""+name,
+        hp:+hp,
+        img:""+img,
+        weapon,
+        attack:()=>`${name} Fight...`}
+
+}
+const player1=createPlayer("Scorpion",100,"http://reactmarathon-api.herokuapp.com/assets/scorpion.gif",["flamberg","bastard"]);
+const player2=createPlayer("Kung Lao",70,"http://reactmarathon-api.herokuapp.com/assets/liukang.gif",["claymore"]);
